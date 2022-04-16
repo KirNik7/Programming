c3698071d3fe8f1cb3b5a8ac7a035fd097f7d231
Кирилл Никитин <kirnik2003@mail.ru>
2022-04-16 09:15:04 +0700

    Исправление формулы в CollisionManager.cs

--- src/Programming/Programming/Model/Classes/CollisionManager.cs
+++ src/Programming/Programming/Model/Classes/CollisionManager.cs
@@ -8,8 +8,8 @@ namespace Programming.Model.Classes
         {
             int dX = Math.Abs(rectangle1.Center.X - rectangle2.Center.X);
             int dY = Math.Abs(rectangle1.Center.Y - rectangle2.Center.Y);
-            double widthDifference = Math.Abs(rectangle1.Width - rectangle2.Width) / 2;
-            double lengthDifference = Math.Abs(rectangle1.Length - rectangle2.Length) / 2;
+            double widthDifference = Math.Abs(rectangle1.Width + rectangle2.Width) / 2;
+            double lengthDifference = Math.Abs(rectangle1.Length + rectangle2.Length) / 2;
 
             return (dX < widthDifference) && (dY < lengthDifference);
         }
