d1a5900b0343e58cd63bc3be6b15a92c1a496bae
Кирилл Никитин <kirnik2003@mail.ru>
2022-12-23 19:24:38 +0700

    Lab 4

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/Model/PriorityOrder.cs
@@ -0,0 +1,20 @@
+﻿using ObjectOrientedPractics.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Хранит данные о заказе приоритетного обслуживания.
+    /// </summary>
+    public class PriorityOrder
+    {
+        private readonly int _id;
+        private DateTime _date;
+        private OrderStatus orderStatus;
+
+    }
+}
