ec9ebae4b0e6f6c76229c367ba9e23ddbaa5c225
Кирилл Никитин <kirnik2003@mail.ru>
2023-03-22 03:02:50 +0700

    Lab 2 done

---
+++ src/Contacts/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Contacts.ViewModel;
+using System.Windows;
+
+namespace Contacts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainVM vm = new MainVM();
+            DataContext = vm;
+        }
+    }
+}
---
+++ src/Contacts/Model/Contact.cs
@@ -0,0 +1,44 @@
+﻿namespace View.Model
+{
+    /// <summary>
+    /// Хранит данные о контакте.
+    /// </summary>
+    public class Contact
+    {
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="name">Имя контакта.</param>
+        /// <param name="email">Электронная почта контакта.</param>
+        /// <param name="phone">Номер телефона контакта.</param>
+        public Contact(string name, string email, string phone)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+        
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        public Contact()
+        {
+
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт имя контакта.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Возвращает и задаёт электронную почту контакта.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Возвращает и задаёт номер телефона контакта.
+        /// </summary>
+        public string Phone { get; set; }
+    }
+}
